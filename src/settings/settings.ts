8c90b429d57d410801bd23b4e9c0c4825703fe7b
Michał Trąbski <michaltrabskiport@gmail.com>
2021-09-14 09:33:13 +0200

    added new path to media

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,6 +1,10 @@
 export const APPTITLE = "AudioBooks App";
+// export const FOLDERWITHAUDIOBOOKS =
+//   "https://dacmwwxjyw.cfolks.pl/files/audiobooks/";
+
 export const FOLDERWITHAUDIOBOOKS =
-  "https://dacmwwxjyw.cfolks.pl/files/audiobooks/";
+  "https://files.dacmwwxjyw.cfolks.pl/audiobooks/";
+
 export const ENDPOINT = FOLDERWITHAUDIOBOOKS + "api/";
 export const marksStep = 60 * 4; // it marks steps on slider every 5 min
 export const limitStep = 5; // it is how many more books will show on show more button click
